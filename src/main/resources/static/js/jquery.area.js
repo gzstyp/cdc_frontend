c222419b1481896d8c09e3125e9ff8d6e6a5fb73
gzstyp <444141300@qq.com>
2020-12-21 21:30:23 +0800

    省市县级联操作

---
+++ src/main/resources/static/js/jquery.area.js
@@ -0,0 +1,111 @@
+;(function($){
+    var clsProvince = '.clsProvince';
+    var clsCity = '.clsCity';
+    var clsCounty = '.clsCounty';
+    var clsTowns = '.clsTowns';
+    var clsVallage = '.clsVallage';
+    var clsXxx = '.clsXxx';
+    window.jQSelect = {
+        renderSelect(data,containerDom,selectDom,labelText){
+            var html = "<option value=''>"+labelText+"</option>";
+            $.each(data,function(index,item){
+                html += "<option value='"+item.id+"'>"+item.name+"</option>";
+            });
+            $(containerDom +' '+ selectDom).html(html);
+            this.displayShow(containerDom,selectDom);
+            this.displayHide(containerDom,selectDom);
+            this.setValue(containerDom,selectDom);
+        },
+        displayShow : function(containerDom,selectDom){
+            $(containerDom +' '+ selectDom).css({"display":"inline"});
+        },
+        displayHide : function(containerDom,selectDom){
+            if(selectDom == clsProvince){
+                $(containerDom +' '+ clsCity).css({"display":"none"});
+                $(containerDom +' '+ clsCounty).css({"display":"none"});
+                $(containerDom +' '+ clsTowns).css({"display":"none"});
+                $(containerDom +' '+ clsVallage).css({"display":"none"});
+                $(containerDom +' '+ clsXxx).css({"display":"none"});
+            }else if(selectDom == clsCity){
+                $(containerDom +' '+ clsCounty).css({"display":"none"});
+                $(containerDom +' '+ clsTowns).css({"display":"none"});
+                $(containerDom +' '+ clsVallage).css({"display":"none"});
+                $(containerDom +' '+ clsXxx).css({"display":"none"});
+            }else if(selectDom == clsCounty){
+                $(containerDom +' '+ clsTowns).css({"display":"none"});
+                $(containerDom +' '+ clsVallage).css({"display":"none"});
+                $(containerDom +' '+ clsXxx).css({"display":"none"});
+            }else if(selectDom == clsTowns){
+                $(containerDom +' '+ clsVallage).css({"display":"none"});
+                $(containerDom +' '+ clsXxx).css({"display":"none"});
+            }else if(selectDom == clsVallage || selectDom == clsXxx){
+                $(containerDom +' '+ clsXxx).css({"display":"none"});
+            }
+        },
+        setValue : function(containerDom,selectDom){
+            if(selectDom == clsProvince){
+                $(containerDom +' '+ clsCity).val('');
+                $(containerDom +' '+ clsCounty).val('');
+                $(containerDom +' '+ clsTowns).val('');
+                $(containerDom +' '+ clsVallage).val('');
+                $(containerDom +' '+ clsXxx).val('');
+            }else if(selectDom == clsCity){
+                $(containerDom +' '+ clsCounty).val('');
+                $(containerDom +' '+ clsTowns).val('');
+                $(containerDom +' '+ clsVallage).val('');
+                $(containerDom +' '+ clsXxx).val('');
+            }else if(selectDom == clsCounty){
+                $(containerDom +' '+ clsTowns).val('');
+                $(containerDom +' '+ clsVallage).val('');
+                $(containerDom +' '+ clsXxx).val('');
+            }else if(selectDom == clsTowns){
+                $(containerDom +' '+ clsVallage).val('');
+                $(containerDom +' '+ clsXxx).val('');
+            }else if(selectDom == clsVallage || selectDom == clsXxx){
+                $(containerDom +' '+ clsXxx).val('');
+            }
+        },
+        getAreaData : function(pid,containerDom,selectDom,labelText){
+            if(pid == null || pid == ''){
+                this.displayHide(containerDom,selectDom);
+                this.setValue(containerDom,selectDom);
+                $(containerDom +' '+ selectDom).val('');
+                $(containerDom +' '+ selectDom).css({"display":"none"});
+                return;
+            }
+            var _self = this;
+            self.layerIndex = layerFn.loading('正在加载……');
+            $.ajax({
+                type : "GET",
+                url : urlPrefix + '/user/queryAreaSelect',
+                dataType : "json",
+                data : {pId : pid},
+                headers : {'accessToken': sessionStorage.getItem('accessToken') || '',"refreshToken":sessionStorage.getItem('refreshToken') || ''},//好使
+                crossDomain: true == !(document.all),
+                success : function(data){
+                    if(data.code === AppKey.code.code200){
+                        _self.renderSelect(data.data,containerDom,selectDom,labelText);
+                    }else if(data.code === AppKey.code.code205){
+                        layerFn.tokenLogin();
+                    }
+                },
+                error : function(response,status){},
+                statusCode : {
+                    401 : function(response){
+                        layerFn.handleClose("没有操作权限");
+                    },
+                    404 : function(response){
+                        var json = eval('('+ response.responseText +')');
+                        layerFn.handleClose("请求("+json.path+")路径不存在");
+                    },
+                    500 : function(response){
+                        layerFn.handleClose("系统出现错误,稍候重试");
+                    }
+                },
+                complete : function(response,status){
+                    layerFn.closeIndex(self.layerIndex);
+                }
+            });
+        }
+	};
+})(jQuery);
